184950ea3080ab7ef7c06e0fb29dbd3468620f66
Josh <josh_alot@protonmail.com>
2026-03-26 21:47:54 -0300

    update main.go
    update main.go

--- main.go
+++ main.go
@@ -2,22 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	t "github.com/go_task_tracker/tasks"
 )
 
 func main() {
-	//tasks := []t.Task{
-	//	{ID: 1, Description: "Go shopping", Status: 0},
-	//	{ID: 2, Description: "Sweep house", Status: 1},
-	//	{ID: 3, Description: "Feed kitten", Status: 2},
-	//}
-	//
-	//err := t.CreateTask(tasks, "tasks.json")
-	//if err != nil {
-	//	fmt.Println("Error:", err)
-	//}
-
-	tasks, _ := t.ListIncompleteTasks("tasks.json")
-	fmt.Println(tasks)
+	fmt.Printf("Go Task Tracker CLI")
 }
